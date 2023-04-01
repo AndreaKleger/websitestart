0fd92c4103cb7f225181242f9f566b7359a5c384
Andrea Kleger <andrea.kleger@gmx.ch>
2023-04-01 20:37:02 +0200

    json data added

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
+let globalData = [];
 
+async function fetchJsonData(path) {
+  const response = await fetch(path);
+  const data = await response.json();
+  return data;
+}
+
+fetchJsonData("./resources/data.json").then((data) => {
+  globalData = data;
+  let me = 2;
+  console.log(globalData);
+});
